e895e5c36e8d58e9a495fcd88e922bd3c84dfa97
majid shahbaz <majidshahbazg@gmail.com>
2026-05-08 15:14:18 +0100

    Initial Commit

---
+++ IOS-proj-2/IOS_proj_2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_proj_2App.swift
+//  IOS-proj-2
+//
+//  Created by Majid on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS_proj_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-proj-2Tests/IOS_proj_2Tests.swift
@@ -0,0 +1,18 @@
+//
+//  IOS_proj_2Tests.swift
+//  IOS-proj-2Tests
+//
+//  Created by Majid on 08/05/2026.
+//
+
+import Testing
+
+struct IOS_proj_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
